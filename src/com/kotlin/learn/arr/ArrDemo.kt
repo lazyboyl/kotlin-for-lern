542f1a7f84dc0eef45cd9157d6f2566e2ff85646
282245889@qq.com <575088Wuling>
2019-09-29 00:21:04 +0800

    二位数组翻转

--- src/com/kotlin/learn/arr/ArrDemo.kt
+++ src/com/kotlin/learn/arr/ArrDemo.kt
@@ -11,7 +11,7 @@ class ArrDemo {
         @JvmStatic
         fun main(args: Array<String>) {
             var arrDemo = ArrDemo()
-            var array = Array(4, { y -> Array(4, { x -> Blob(x, y, 0) }) })
+            var array = arrDemo.createArray()
             array[3][2].value = 8
             array[2][2].value = 4
             arrDemo.dumpArray(array, "初始化以后数组情况")
@@ -19,9 +19,16 @@ class ArrDemo {
             arrDemo.dumpArray(array, "旋转以后数组情况")
             arrDemo.rotateArray(array, "bottom")
             arrDemo.dumpArray(array, "旋转回来数组情况")
+            arrDemo.rotateArray(array, "left")
+            arrDemo.dumpArray(array, "对调以后数组情况")
 
         }
     }
+
+    /**
+     * 功能描述： 构建二维数组
+     */
+    fun createArray() = Array(4) { y -> Array(4) { x -> Blob(x, y, 0) } }
 
     /**
      * 功能描述： 实现数组方向的旋转
@@ -30,11 +37,14 @@ class ArrDemo {
      */
     fun rotateArray(array: Array<Array<Blob>>, direction: String) {
         when (direction) {
+            "left" ->{
+                covery(array, { x: Int, y: Int, size: Int -> size - 1 - x }, { x: Int, y: Int, size: Int -> y });
+            }
             "top" -> {
                 covery(array, { x: Int, y: Int, size: Int -> size - 1 - y }, { x: Int, y: Int, size: Int -> x })
             }
-            "bottom" ->{
-                covery(array, { x: Int, y: Int, size: Int -> y }, { x: Int, y: Int, size: Int -> size - 1 - x  })
+            "bottom" -> {
+                covery(array, { x: Int, y: Int, size: Int -> y }, { x: Int, y: Int, size: Int -> size - 1 - x })
             }
         }
     }
@@ -52,15 +62,15 @@ class ArrDemo {
     ) {
         var changeArray = Array(4) { y -> Array(4) { 0 } }
         var size = array.size
-        for(y in 0 until size){
-            for(x in 0 until size){
-               if(changeArray[y][x]==1){
-                   continue
-               }
+        for (y in 0 until size) {
+            for (x in 0 until size) {
+                if (changeArray[y][x] == 1) {
+                    continue
+                }
                 var temp = array[y][x]
-                array[y][x] = array[coverY(x,y,size)][coverX(x,y,size)]
-                array[coverY(x,y,size)][coverX(x,y,size)] = temp
-                changeArray[coverY(x,y,size)][coverX(x,y,size)] = 1
+                array[y][x] = array[coverY(x, y, size)][coverX(x, y, size)]
+                array[coverY(x, y, size)][coverX(x, y, size)] = temp
+                changeArray[coverY(x, y, size)][coverX(x, y, size)] = 1
 
             }
         }
